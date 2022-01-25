7cb9ca9285b7fb41d7a0af43d38b9853f67cd276
CeAlCuadrado <cealcuadrado@outlook.com>
2022-01-25 15:20:36 -0300

    Creada O3

---
+++ src/app/layout/option-three/option-three.component.ts
@@ -0,0 +1,20 @@
+import { Post } from 'src/app/interfaces/post';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-option-three',
+  templateUrl: './option-three.component.html',
+  styleUrls: ['./option-three.component.scss']
+})
+export class OptionThreeComponent implements OnInit {
+
+  @Input() posts: Post[] = [];
+  @Input() itemsPerPage: number = 10;
+  page = 1;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
